00ae2fed0aa7cb950e88ccd5c3d65eb4b798b93a
JCarlosOrtega <j.karlox@gmail.com>
2023-02-02 12:04:03 +0100

    Enumerado especie

--- src/repoblacion/modelo/Especie.java
+++ src/repoblacion/modelo/Especie.java
@@ -2,4 +2,23 @@ package repoblacion.modelo;
 
 public enum Especie {
 
+	ALAMO("Álamo"), ENCINA("Encina"), CASTANO("Castaño"), CIPRES("Ciprés"), PINO("Pino"), ROBLE("Roble"),
+	OLIVO("Olivo");
+	
+	public String cadenaAMostrar;
+	
+	private Especie(String cadenaMostrada) {
+		
+		this.cadenaAMostrar = cadenaMostrada;
+		
+	}
+
+	@Override
+	public String toString() {
+		
+		return cadenaAMostrar;
+	}
+	
+	
+	
 }
