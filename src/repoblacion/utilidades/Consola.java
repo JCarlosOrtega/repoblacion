28a86e8d669b83351f6c3ebb76a4feb5d9a7e0b6
JCarlosOrtega <j.karlox@gmail.com>
2023-02-06 00:44:54 +0100

    Clase consola

--- src/repoblacion/utilidades/Consola.java
+++ src/repoblacion/utilidades/Consola.java
@@ -1,5 +1,35 @@
 package repoblacion.utilidades;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
+
+	private Consola() {
+
+	}
+
+	public static int leerAnchura() {
+
+		System.out.println("Introduce la anchura del bosque: ");
+		int ancho = Entrada.entero();
+
+		return ancho;
+	}
+
+	public static int leerAltura() {
+
+		System.out.println("Introduce la altura del bosque: ");
+		int alto = Entrada.entero();
+
+		return alto;
+	}
+
+	public static int leerPoblacion() {
+
+		System.out.println("Introduce el número de la población de especies: ");
+		int poblacion = Entrada.entero();
+
+		return poblacion;
+	}
 
 }
